8db0a4e925f60f8b1055f8f4d9260153d0c35dd7
Pranjali <pranjalik111@gmail.com>
2026-06-03 17:41:45 -0400

    Update frontend to use EC2 endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
     setResults(null);
 
     try {
-      const res = await fetch("http://127.0.0.1:8001/generate", {
+      const res = await fetch("http://3.21.168.210:8001/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ spec_path: specPath }),
